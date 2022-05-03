0176190d3c493f750702d67b96206b7fbfeebd0b
Cristian Romero <mascrit@gmail.com>
2022-05-02 22:20:57 -0500

    Ejemplo de uso de rutas

--- index.php
+++ index.php
@@ -1,9 +1,29 @@
 <?php
 session_start();
 
-// TODO: Automatic require for extension *.class.php
-require_once 'conf/constants.php';
-require_once 'recetario/Model/Usuario.class.php';
-require_once 'recetario/Model/DataBase.class.php';
 
-require_once("recetario/view/panel.view.php");
+require_once 'conf/constants.php';
+
+
+require_once 'recetario/Model/Router.php';
+
+use Model\Router;
+
+$router = new Router();
+
+// Index
+$router->get('/', function () { require_once __DIR__.'/recetario/view/panel.view.php';
+});
+
+// Ejemplo con parametros en GET
+$router->get('/about', function (array $params) {
+    echo 'About';
+    echo '<h1>'. $params['parametro']. '</h1>';
+});
+
+$router->add_not_found_handler(function () {
+    $title = '404 No se encuentra lo que buscas, verifica la URL';
+    require_once __DIR__.'/recetario/view/404.view.php';
+});
+
+$router->run();

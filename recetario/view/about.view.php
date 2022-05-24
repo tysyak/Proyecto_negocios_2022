e48276972ba7f22b17e238bda5667084b3a0e6cc
Cristian Romero <mascrit@gmail.com>
2022-05-24 00:23:44 -0500

    Añado choro

--- recetario/view/about.view.php
+++ recetario/view/about.view.php
@@ -1,2 +1,19 @@
-<h2>Misión</h2>
-<p>Nuestra misión es ser los más inteligentes. y la plataforma de alimentos más útil en existencia</p>
+<div class="card">
+
+<div class="container">
+    <h2>Misión</h2>
+    <p>Nuestra misión es ser la plataforma de alimentos más inteligente y útil que existe.</p>
+
+    <p>Completamos esta tarea sumando vida en la cocina
+        Para millones de casos de cosineros en todo el mundo.</p>
+
+    <h3>Ayudamos a las personas a saber qué comer en función de sus preferencias e información personal</h3>
+
+    <img src="/recetario/assets/img/mission.png" alt="mission_img">
+
+    <p>Nuestro sistema de descubrimiento personalizado lo aprende mejor para brindarle un mejor servicio, así que.
+        La pregunta "¿Qué hay para cenar?" Respondido incluso antes de que se hagan las preguntas.</p>
+
+</div></div>
+
+

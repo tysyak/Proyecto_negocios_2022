1646c62a334b8985456bdeb45fc2ae09fbd04c20
Cristian Romero <mascrit@gmail.com>
2022-05-29 14:22:08 -0500

    vision

--- recetario/view/about.view.php
+++ recetario/view/about.view.php
@@ -1,20 +1,24 @@
-
 <div class="card-about">
 
-<div class="container">
-    <h2>Misión</h2>
-    <p>Nuestra misión es ser la plataforma de alimentos más inteligente y útil que existe.</p>
+    <div class="container">
+        <h2>Misión</h2>
+        <p>Nuestra misión es ser la plataforma de alimentos más inteligente y útil que existe.</p>
 
-    <p>Completamos esta tarea sumando vida en la cocina
-        Para millones de casos de cosineros en todo el mundo.</p>
+        <p>Completamos esta tarea sumando vida en la cocina
+            Para millones de casos de cosineros en todo el mundo.</p>
 
-    <h3>Ayudamos a las personas a saber qué comer en función de sus preferencias e información personal</h3>
+        <h3>Ayudamos a las personas a saber qué comer en función de sus preferencias e información personal</h3>
 
-    <img src="/recetario/assets/img/mission.png" alt="mission_img" style="max-width: 40%; height: auto; width: auto\9; border-radius:20px;" >
+        <img src="/recetario/assets/img/mission.png" alt="mission_img" style="max-width: 40%; height: auto; width: auto\9; border-radius:20px;">
 
-    <p>Nuestro sistema de descubrimiento personalizado lo aprende mejor para brindarle un mejor servicio, así que.
-        La pregunta "¿Qué hay para cenar?" Respondido incluso antes de que se hagan las preguntas.</p>
+        <p>Nuestro sistema de descubrimiento personalizado lo aprende mejor para brindarle un mejor servicio, así que.
+            La pregunta "¿Qué hay para cenar?" Respondido incluso antes de que se hagan las preguntas.</p>
 
-</div></div>
+        <hr style="border-top: 1px dashed #FE8019;">
+        <br>
+        <h2>Vision</h2>
+        <br>
+        <i>Ser la plataforma de recetas más inteligente, accesible y variada que se pueeda encontrar.</i>
 
-
+    </div>
+</div>
